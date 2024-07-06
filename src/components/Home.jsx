419c7e80e6eb14654ba663c9725e7dbac4af8084
Nithin-2005-dev <bingu_ug_23@ee.nits.ac.in>
2024-07-06 23:41:35 +0530

    ...

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -14,7 +14,7 @@ const Home = () => {
   const {roboOn,setRobo}=useContext(AnimationStore)
   return (<>
     <section>
-    <header><Header/></header>
+    <header><Header/><button className='absolute top-0 left-0 text-white text-4xl m-3 rotate-90'>{!roboOn?<MdOutlinePower onClick={()=>{setRobo(!roboOn)}} className=''/>:<MdOutlinePowerOff  onClick={()=>{setRobo(!roboOn)}} className='text-red-600'/>}</button></header>
      <motion.div className="flex flex-wrap"
      initial={{opacity:0,y:window.innerHeight,
      }}
@@ -29,14 +29,13 @@ const Home = () => {
      >
     <Suspense fallback={<Loader/>}>
     <Canvas style={{height:"90vh"}}>
-    <HelloRobo scale={6} />
+    <HelloRobo scale={window.innerWidth<600?4:6} />
     <Environment preset='studio'/>
     </Canvas>
     {roboOn && <div style={{width:"50vw",right:"30%"}} className='absolute top-1/4 bg-opacity-70 bg-gray-800'>
-      <p className='text-lg text-emerald-50 font-bold z-30 border-2 p-4 bodoni-moda-sc shadow-md shadow-slate-50 robot md:text-2xl'>Hello, I'm  Nithin. I am currently pursuing a B.Tech in Electrical Engineering at NIT Silchar. I have a strong interest in web development. I am eager to continue exploring and growing in the web development domain.
+      <p className='text-sm text-emerald-50 font-bold z-30 border-2 p-4 bodoni-moda-sc shadow-md shadow-slate-50 robot md:text-2xl'>Hello, I'm  Nithin. I am currently pursuing a B.Tech in Electrical Engineering at NIT Silchar. I have a strong interest in web development. I am eager to continue exploring and growing in the web development domain.
 </p>
     </div>}
-    <button className='absolute top-0 left-0 text-white text-4xl m-3 rotate-90'>{!roboOn?<MdOutlinePower onClick={()=>{setRobo(!roboOn)}} className=''/>:<MdOutlinePowerOff  onClick={()=>{setRobo(!roboOn)}} className='text-red-600'/>}</button>
     </Suspense>
       </motion.div> 
     </section>
