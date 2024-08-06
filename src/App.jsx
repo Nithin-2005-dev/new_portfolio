82e860a93e4d0f26ee30684d316489515224786e
[nithin608] <bingu_ug_23@ee.nits.ac.in>
2024-08-06 17:51:35 +0530

    sending

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import {BrowserRouter as Router} from 'react-router-dom'
 import Loader from './components/Loader'
 import AnimatePages from './components/AnimatePages'
 const App = () => {
+  //app starts from here
   return (
     <>
     <main className='bg-slate-900'>
